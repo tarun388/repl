f14282b98c4dffa3ddd7a4d8008775e8cd4dad4b
Tarun Verma <36099367+tarun388@users.noreply.github.com>
2024-02-17 22:38:48 +0530

    java init

---
+++ executor/src/main/java/com/verma/tarun/executor/executorConfiguration.java
@@ -0,0 +1,10 @@
+package com.verma.tarun.executor;
+
+import io.dropwizard.core.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import jakarta.validation.constraints.*;
+
+public class executorConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
